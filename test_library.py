ba189ef7e25a22bbea09a5b1e2a259c6754cb7e1
Roy <royeo91@gmail.com>
2020-02-13 11:45:26 -0800

    Update test_library.py
    Co-Authored-By: AdrianChan99 <adrianchan99@users.noreply.github.com>
    Co-Authored-By: nathan-broyles <nathan-broyles@users.noreply.github.com>

--- test_library.py
+++ test_library.py
@@ -3,6 +3,7 @@ import unittest
 # from podcast import Podcast
 # from audio_file import AudioFile
 import inspect
+from abc import abstractmethod
 
 class TestLibrary(unittest.TestCase):
     """A class that holds the tests to be run
@@ -16,8 +17,7 @@ class TestLibrary(unittest.TestCase):
     def setUp(self):
         """Sets up each test by initializing default objects"""
         self.logPoint()
-        self.librarydefault = Library(args)
-        pass
+        self.library_default = Library(args)
 
     def tearDown(self):
         """Introduces an end log point for each test"""
@@ -30,5 +30,15 @@ class TestLibrary(unittest.TestCase):
         callingFunction = inspect.stack()[1][3]
         print('in %s - %s()' % (currentTest, callingFunction))
 
-    def test_constructor_valid(self):
+    def test_library_construction(self):
         """TP-010A Tests successful creation of a library class"""
+        self.assertIsInstance(self.library_default, Library)
+
+    def test_constructor_valid(self):
+        """TP-010B Tests for a valid parameter """
+        self.assertRaises(ValueError, Library, 'test', 'test')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
