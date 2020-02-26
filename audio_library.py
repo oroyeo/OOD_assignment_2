eb8fdb44145663f5fa253876ea9a3ac61599474a
Roy <royeo91@gmail.com>
2020-02-25 20:31:00 -0800

    Update audio_library.py

--- audio_library.py
+++ audio_library.py
@@ -29,6 +29,37 @@ class AudioLibrary():
         self._artists = []
 
 
+    def __str__(self) -> str:
+        """Could be the 'get_library_info' function since it returns the formatted string"""
+        description = ''
+        if len(self._songs) == 1:
+            description = "The library '{}' has {} audio files, ({} song and {} podcasts)"\
+                .format(self._library_name,
+                       len(self._audio_files),
+                       len(self._songs),
+                       len(self._podcasts))
+        if len(self._podcasts) == 1:
+            description = "The library '{}' has {} audio files, ({} songs and {} podcast)"\
+                .format(self._library_name,
+                       len(self._audio_files),
+                       len(self._songs),
+                       len(self._podcasts))
+        if len(self._podcasts) == 1 and len(self._songs) == 1:
+            description = "The library '{}' has {} audio files, ({} song and {} podcast)"\
+                .format(self._library_name,
+                       len(self._audio_files),
+                       len(self._songs),
+                       len(self._podcasts))
+
+        else:
+            if len(self._podcasts) == 1:
+                description = "The library '{}' has {} audio files, ({} songs and {} podcasts)" \
+                    .format(self._library_name,
+                            len(self._audio_files),
+                            len(self._songs),
+                            len(self._podcasts))
+        return description
+
     @staticmethod
     def __validate_library(library_name):
         """Validates library parameter argument"""
@@ -50,20 +81,32 @@ class AudioLibrary():
 
     def add_audio_file(self, filename):
         """Adds an audio file to the library and its respective subclass list"""
-        if type(filename) == AudioFile and filename not in self._audio_files:
+        if not isinstance(filename, AudioFile):
+            raise ValueError('Entered filename is not an audiofile')
+        if filename not in self._audio_files:
             self._audio_files.append(filename)
-            if type(filename) == Song and filename not in self._songs:
-                self._songs.append(filename)
-            elif type(filename) == Podcast and filename not in self._podcasts:
-                self._podcasts.append(filename)
+            self.__add_song_helper(filename)
+            self.__add_podcast_helper(filename)
         else:
-            raise ValueError('Entered filename is not an audio file')
+            print('File already exists in your library')
+
+    def __add_song_helper(self, filename):
+        """Adds the file to the song list if it is a song"""
+        if isinstance(filename, Song):
+            if filename not in self._songs:
+                self._songs.append(filename)
+
+    def __add_podcast_helper(self, filename):
+        """Adds the file to the song list if it is a song"""
+        if isinstance(filename, Podcast):
+            if filename not in self._podcasts:
+                self._podcasts.append(filename)
 
     def remove_audio_file(self, filename):
         """Removes an audio file from the library and its respective subclass list"""
         if type(filename) == AudioFile and filename in self._audio_files:
             self._audio_files.remove(filename)
-            if type(filename) == Song and filename not self._songs:
+            if type(filename) == Song and filename in self._songs:
                 self._songs.remove(filename)
             elif type(filename) == Podcast and filename in self._podcasts:
                 self._podcasts.remove(filename)
@@ -71,6 +114,25 @@ class AudioLibrary():
             raise ValueError('Entered filename is not an audio file')
 
 
+    def add_playlist(self, playlist):
+        """Adds a playlist to the library"""
+        if isinstance(playlist, Playlist):
+            if playlist not in self._playlists:
+                self._playlists.append(playlist)
+            else:
+                print('Playlist already exists')
+        else:
+            raise ValueError('entered value is not a playlist')
+
+    def remove_playlist(self, playlist):
+        """Removes a playlist from the library"""
+        if isinstance(playlist, Playlist):
+            if playlist in self._playlists:
+                self._playlists.remove(playlist)
+            else:
+                print('That playlist is not in your existing playlists')
+        else:
+            raise ValueError('argument is not a playlist object')
 
 
 
