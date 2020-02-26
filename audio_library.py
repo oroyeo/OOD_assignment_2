c2329354f469a56dc7eb59defd9f8ad0bc033c10
Roy <royeo91@gmail.com>
2020-02-25 18:51:46 -0800

    Update audio_library.py

--- audio_library.py
+++ audio_library.py
@@ -5,7 +5,7 @@ from song import Song
 from playlist import Playlist
 from podcast import Podcast
 
-class Audio_Library():
+class AudioLibrary():
     """ Represents an abstract audio library, this is a subclass of the
         AudioFile class
 
@@ -47,6 +47,28 @@ class Audio_Library():
             raise ValueError('new library name should be a string')
         else:
             self._library_name = new_name
+
+    def add_audio_file(self, filename):
+        """Adds an audio file to the library and its respective subclass list"""
+        if type(filename) == AudioFile and filename not in self._audio_files:
+            self._audio_files.append(filename)
+            if type(filename) == Song and filename not in self._songs:
+                self._songs.append(filename)
+            elif type(filename) == Podcast and filename not in self._podcasts:
+                self._podcasts.append(filename)
+        else:
+            raise ValueError('Entered filename is not an audio file')
+
+    def remove_audio_file(self, filename):
+        """Removes an audio file from the library and its respective subclass list"""
+        if type(filename) == AudioFile and filename in self._audio_files:
+            self._audio_files.remove(filename)
+            if type(filename) == Song and filename not self._songs:
+                self._songs.remove(filename)
+            elif type(filename) == Podcast and filename in self._podcasts:
+                self._podcasts.remove(filename)
+        else:
+            raise ValueError('Entered filename is not an audio file')
 
 
 
