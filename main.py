6efd388546d7c43ed676a2c408d08e8f8f3f8153
Roy <royeo91@gmail.com>
2020-02-25 20:30:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from audio_library import AudioLibrary
+from song import Song
+from audio_file import AudioFile
+from podcast import Podcast
 
 
 def main():
     library1 = AudioLibrary('libarry')
     print(library1.library_name)
     library1.library_name = '12312312321'
-    print(library1.library_name)
-
+    song1 = Song('Crazy', 'Gnarls Barkley', '3:02', r".\Music\Crazy.mp3", 'St. Elsewhere')
+    print(type(song1))
+    print(library1)
+    library1.add_audio_file(song1)
+    print(library1)
+    print(library1._songs, library1._audio_files)
 
 
 
