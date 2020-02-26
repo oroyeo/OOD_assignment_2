c0c47282d4a630f983157d567869aff31f157ed0
Roy <royeo91@gmail.com>
2020-02-25 18:03:27 -0800

    Update audio_library.py

--- audio_library.py
+++ audio_library.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 from datetime import datetime, time
 from audio_file import AudioFile
 from song import Song
@@ -17,10 +17,40 @@ class Audio_Library():
     _PROGRESS_FORMAT = "%M"
     _DATE_FORMAT = "%Y-%m-%d"
 
-    def __init__(self, title: str, artist: str, runtime: str, path_name: str, series: str,
-                 episode_date: datetime, season: str = None, episode_number: int = None):
-        """Creates an object instance of the song subclass"""
+    def __init__(self, library_name: str):
+        """Creates an object instance of a library class"""
+        self._library_name = library_name
+        self._liked_audio_files = []
+        self._audio_files = []
+        self._songs = []
+        self._podcasts = []
+        self._playlists = []
+        self._recently_played = []
+        self._artists = []
+
+
+    @staticmethod
+    def __validate_library(library_name):
+        """Validates library parameter argument"""
+        if type(library_name) != str:
+            raise ValueError('library name must be a string')
+
+    @property
+    def library_name(self) -> str:
+        """Gets the current library name for this object instance"""
+        return self._library_name
+
+    @library_name.setter
+    def library_name(self, new_name: str):
+        """Sets a new name for this current object instance"""
+        if type(new_name) != str:
+            raise ValueError('new library name should be a string')
+        else:
+            self._library_name = new_name
 
 
 
-    
+
+
+
+
